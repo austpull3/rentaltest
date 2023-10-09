ad0020fec2251bf57b58bc55c5fc751fecf831c5
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-09 15:59:22 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
+import sys
+
+# Display the Python version
+st.write(f"Python Version: {sys.version}")
+
 import pyodbc
 st.title("Hello")
 st.title("Welcome")
