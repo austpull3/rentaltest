314356ce5c1edbab2e029719a6b09a9a4090e03e
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-09 16:16:12 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,11 +1,20 @@
 import streamlit as st
-import pandas as pd
-import streamlit as st
-import sys
-
-# Display the Python version
-st.write(f"Python Version: {sys.version}")
-
+#import necessary libraries
 import pyodbc
-st.title("Hello") 
-st.title("Welcome")
+import pandas as pd
+
+server = 'HOWE-ACCT-SERV\SAGE300CRE'
+database = 'HoweInc'
+username = 'sa'
+password = 'L+S3jr3fP@*1'
+
+# Create a connection string
+connection_string = f"DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
+# Establish a connection
+conn = pyodbc.connect(connection_string)
+
+cursor = conn.cursor()
+
+query = "USE [HoweInc]"
+result = cursor.execute(query)
